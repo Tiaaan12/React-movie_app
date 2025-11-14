77a5d6b084e433ce64eaa5a70f345f3c8f62fa67
tian-devera <deverachristian232@gmail.com>
2025-11-14 22:53:02 +0800

    Initiailizig my files

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { Link } from "expo-router/build/link/Link";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-dark-100 font-bold">
+          Welcome to my app.</Text>
+    </View>
+  );
+}
