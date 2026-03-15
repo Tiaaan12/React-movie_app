7b50dd836d23f933c38b447ae621e2cead8a0d8b
Tiaaan12 <christiandevera232@gmail.com>
2026-03-15 19:10:04 +0800

    minor changes

--- components/TrendingCard.tsx
+++ components/TrendingCard.tsx
@@ -29,6 +29,12 @@ const TrendingCard = ({
             />
           </MaskedView>
         </View>
+        <Text
+          className="text-sm font-bold mt-2 text-light-200"
+          numberOfLines={2}
+        >
+          {title}
+        </Text>
       </TouchableOpacity>
     </Link>
   );
