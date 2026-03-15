46a29c3cc57c77a989d01c604d83a0fecdb71bfc
Tiaaan12 <christiandevera232@gmail.com>
2026-03-15 19:55:51 +0800

    added budget revenue and production companies

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -60,7 +60,22 @@ const MovieDetails = () => {
             label="Genres"
             value={movie?.genres?.map((g) => g.name).join("-") || "N/A"}
           />
-          <View className="flex flex-row"></View>
+          <View className="flex flex-row justify-between w-1/2">
+            <MovieInfo
+              label="Budget"
+              value={`$${movie?.budget / 1_000_000} million`}
+            />
+            <MovieInfo
+              label="Revenue"
+              value={`$${Math.round(movie?.revenue) / 1_000_000}`}
+            />
+          </View>
+          <MovieInfo
+            label="Production Companies"
+            value={
+              movie?.production_companies.map((c) => c.name).join("-") || "N/A"
+            }
+          />
         </View>
       </ScrollView>
     </View>
