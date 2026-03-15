5f1bb07f22849e12b9a3838194df5f33cb201e47
Tiaaan12 <christiandevera232@gmail.com>
2026-03-15 19:33:55 +0800

    creating movieDetails component

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,11 +1,30 @@
-import { Text, View } from "react-native";
-import React from 'react'
+import { fetchMovieDetails } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import { useLocalSearchParams } from "expo-router";
+import React from "react";
+import { Image, ScrollView, View } from "react-native";
 
 const MovieDetails = () => {
-    return (
+  const { id } = useLocalSearchParams();
+
+  const { data: movie, loading } = useFetch(() =>
+    fetchMovieDetails(id as string),
+  );
+
+  return (
+    <View className="bg-primary flex-1">
+      <ScrollView contentContainerStyle={{ paddingBottom: 80 }}>
         <View>
-            <Text>MovieDetails</Text>
+          <Image
+            source={{
+              uri: `https://image.tmdb.org/t/p/w500${movie?.poster_path}`,
+            }}
+            className="w-full h-[550px]"
+            resizeMode="stretch"
+          />
         </View>
-    )
-}
-export default MovieDetails
+      </ScrollView>
+    </View>
+  );
+};
+export default MovieDetails;
