6b04d8bcd72aa31d335f8af380c8c8e8bf3b33ca
Tiaaan12 <christiandevera232@gmail.com>
2026-03-15 20:11:43 +0800

    started background components for profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,11 +1,17 @@
-import { Text, View } from "react-native";
-import React from 'react'
+import { images } from "@/constants/images";
+import React from "react";
+import { Image, Text, View } from "react-native";
 
 const Profile = () => {
-    return (
-        <View>
-            <Text>Profile</Text>
-        </View>
-    )
-}
-export default Profile
+  return (
+    <View className="bg-primary flex-1">
+      <Image
+        source={images.bg}
+        className="w-full flex-1 absolute z-0"
+        resizeMode="cover"
+      />
+      <Text>Profile</Text>
+    </View>
+  );
+};
+export default Profile;
